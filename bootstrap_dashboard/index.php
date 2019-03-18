05fbb7a2b9e6db4a1eb9c02e6560e00a3be2bcdd
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-18 17:37:42 -0400

    Changed card contents

--- bootstrap_dashboard/index.php
+++ bootstrap_dashboard/index.php
@@ -51,7 +51,7 @@
           <span>Investments 101</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="portfolio.php">
           <i class="fas fa-fw fa-book-open"></i>
           <span>My Portfolio</span></a>
       </li>
@@ -367,8 +367,8 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Portfolio Balance</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$5,000</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Portfolio Value</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">$18,000</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -384,11 +384,11 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Profit/Loss</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">+ $1107.25</div>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Portfolio Balance</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">$5,000</div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
@@ -401,20 +401,11 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
-                      <div class="row no-gutters align-items-center">
-                        <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                        </div>
-                        <div class="col">
-                          <div class="progress progress-sm mr-2">
-                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                          </div>
-                        </div>
-                      </div>
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Profit/Loss</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">+ $1107.25</div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
