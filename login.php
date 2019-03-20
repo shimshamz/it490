22930e1132fc6e9389f6a41b45337bdaf3a6730c
root <pap060895@gmail.com>
2019-03-20 12:53:53 -0400

    local auth and backend code

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
 require_once('/home/it490/rabbitmqphp_example/path.inc');
 require_once('/home/it490/rabbitmqphp_example/get_host_info.inc');
 require_once('/home/it490/rabbitmqphp_example/rabbitMQLib.inc');
-
 $client = new rabbitMQClient("/home/it490/rabbitmqphp_example/testRabbitMQ.ini","testServer");
+
 if (isset($argv[1]))
 {
   $msg = $argv[1];
@@ -13,14 +13,11 @@ else
 {
   $msg = "loginnnnnnnnnn";
 }
-
 $request = array();
 $request['type'] = "login";
 #$request['email'] = "user3@gmail.com";
 #$request['password'] = "1234";
-
-
-$request['email'] = $_POST['email'];
+$request['username'] = $_POST['email'];
 $request['password'] = $_POST['pass'];
 $request['message'] = $msg;
 $response = $client->send_request($request);
@@ -28,18 +25,11 @@ $response = $client->send_request($request);
 echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
-
 if ($response == 0 ) {
-
 header("location:loginerror.html");
-
 }
 else {
-
-	header("location:loginsuccess.php");
-
+	$_SESSION['username']= $_POST['username'];
+	header("location:kumartest1.php");
 }
-
-
-
 ?>
