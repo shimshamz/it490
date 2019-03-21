d86122e459ed4757f88fbf680bdf5c3f2732c854
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-20 22:05:07 -0400

    Check if buy amount exceeds available balance

--- bootstrap_dashboard/buy.php
+++ bootstrap_dashboard/buy.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
+$userid = $_SESSION['userid'];
 
-$quantity = $_POST['buy'];
-$symbol = $_SESSION['symbol'];
-$company_name = $_SESSION['name'];
-$price = $_SESSION['currPrice'];
-$dashboard_id = $_SESSION['dashId']; 
 $mydb = new mysqli('127.0.0.1','admin','password','stocks');
 if ($mydb->errno != 0)
 {
@@ -14,13 +10,28 @@ if ($mydb->errno != 0)
 }
 echo "successfully connected to database".PHP_EOL;
 
-$totalAmount = $quantity * $price;
-$query = mysqli_query($mydb,"INSERT INTO portfolio (dashboard_id, company_symbol, company_name, volume, buy_date, buy_amount) VALUES ($dashboard_id, $symbol, $company_name, $quantity, CURDATE(), $price)");
-$dash_info = mysqli_query($mydb,"SELECT balance FROM dashboard");
-$currBal = $dash_info['balance'];
-$newBal = $currBal - $totalAmount;
-$dash_info = mysqli_query($mydb,"UPDATE dashboard SET balance = $newBal WHERE id = $dashboard_id");
-$count = mysqli_num_rows($query);
+$dash_info = mysqli_query($mydb,"SELECT * FROM dashboard WHERE userid = $userid");
 
-header('Location: index.php');
+$quantity = $_POST['buy'];
+$symbol = $_SESSION['symbol'];
+$company_name = $_SESSION['name'];
+$price = $_SESSION['currPrice'];
+$dashboard_id = $dash_info['balance'];
+$currBal = $dash_info['balance'];
+
+$totalAmount = $quantity * $price;
+
+
+$query = mysqli_query($mydb,"INSERT INTO portfolio (dashboard_id, company_symbol, company_name, volume, buy_date, buy_amount) VALUES ($dashboard_id, $symbol, $company_name, $quantity, CURDATE(), $price)");
+
+if ($totalAmount > $currBal) {
+  echo "Total buy amount exceeds balance available."
+}
+else {
+  $newBal = $currBal - $totalAmount;
+  $dash_info = mysqli_query($mydb,"UPDATE dashboard SET balance = $newBal WHERE id = $dashboard_id");
+  
+  header('Location: index.php');
+}
+
 ?>
