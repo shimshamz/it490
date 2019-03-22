0326f9b14532cdbf3563ce1337cb5d6fbe82d9c5
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-21 21:33:52 -0400

    Updated login.php

--- bootstrap_dashboard/login.php
+++ bootstrap_dashboard/login.php
@@ -39,17 +39,16 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user">
+                  <form method="post" action="../login.php" class="user">
                     <div class="form-group">
                       <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                     </div>
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
                     </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
+                    <input type="submit" class="btn btn-primary btn-user btn-block" value="Login">
                     <hr>
+                  </form>
                   <div class="text-center">
                     <a class="small" href="register.php">Create an Account!</a>
                   </div>
