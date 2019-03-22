5d33d8da806fc83f540fd282c4b8e877c1b0a768
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-21 21:34:04 -0400

    Updated register.php

--- bootstrap_dashboard/register.php
+++ bootstrap_dashboard/register.php
@@ -34,7 +34,7 @@
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
               </div>
-              <form class="user">
+              <form method="post" action="../register.php" class="user">
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
@@ -50,14 +50,10 @@
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
                   </div>
-                  <div class="col-sm-6">
-                    <input type="number" class="form-control form-control-user" id="exampleInputAge" placeholder="Age">
-                  </div>
                 </div>
-                <a href="login.html" class="btn btn-primary btn-user btn-block">
-                  Register Account
-                </a>
-              <hr>
+                <input type="submit" class="btn btn-primary btn-user btn-block" value="Register">
+                <hr>
+              </form>
               <div class="text-center">
                 <a class="small" href="login.html">Already have an account? Login!</a>
               </div>
