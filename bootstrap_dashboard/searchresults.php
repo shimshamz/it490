b4bc08ba2f1dfdf2f113446de70e078dcac4844a
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-22 12:25:55 -0400

    Changed search form method to get

--- bootstrap_dashboard/searchresults.php
+++ bootstrap_dashboard/searchresults.php
@@ -194,7 +194,7 @@ $historyarray = json_decode($history_string, true);
           </button>
 
           <!-- Topbar Search -->
-          <form method="post" action="searchresults.php" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+          <form method="get" action="searchresults.php" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div class="input-group">
               <input type="text" name="search" class="form-control bg-light border-0 small" placeholder="Search symbol..." aria-label="Search" aria-describedby="basic-addon2">
               <div class="input-group-append">
