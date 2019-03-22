50141128c620198be28e51365a64178df7908eef
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-22 17:04:21 -0400

    Added chart to display historical data

--- bootstrap_dashboard/searchresults.php
+++ bootstrap_dashboard/searchresults.php
@@ -14,7 +14,7 @@ if (isset($_GET['search']) && !empty($_GET['search'])) {
   $searchname = preg_replace('/\s+/', '+', $search_input);
   //search
   $json_string = file_get_contents("https://marketdata.websol.barchart.com/getQuote.json?apikey=aedef88fae1654cbca88ef03ee28b57e&symbols=".$search_input."");
-  $history_string = file_get_contents("https://marketdata.websol.barchart.com/getHistory.json?apikey=aedef88fae1654cbca88ef03ee28b57e&symbol=".$search_input."&type=daily&startDate=".$sevenDaysAgo."&endDate=".$yesterday."&order=desc");
+  $history_string = file_get_contents("https://marketdata.websol.barchart.com/getHistory.json?apikey=5077e2ecc61ff495ec2387ec9e58cf95&symbol=".$search_input."&type=daily&startDate=".$sevenDaysAgo."&endDate=".$yesterday."&order=desc");
 }
 
 $jsonarray = json_decode($json_string, true); //convert json into multidimensional associative array
@@ -41,12 +41,6 @@ $historyarray = json_decode($history_string, true);
 
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-  <!-- JQuery CDN-->
-  <script
-  src="http://code.jquery.com/jquery-3.3.1.js"
-  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-  crossorigin="anonymous"></script>
 
 </head>
 
@@ -443,6 +437,18 @@ $historyarray = json_decode($history_string, true);
                       
                           <div class="row">
                             <div class="col-lg-12">
+
+                              <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                  <h6 class="m-0 font-weight-bold text-primary">Closing Prices for the Week</h6>
+                                </div>
+                                <div class="card-body">
+                                  <div class="chart-area">
+                                    <canvas id="myLineChart"></canvas>
+                                  </div>
+                                </div>
+                              </div>
+
                               <div class="table-responsive">
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                   <thead>
@@ -642,6 +648,200 @@ $historyarray = json_decode($history_string, true);
   <script src="js/demo/chart-pie-demo.js"></script>
 
   <script src="currencyconverter.js"></script>
+
+<script>
+    // Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+Chart.defaults.global.animation.duration = 0;
+
+
+var values = getChartData('<?php echo $search_input ?>');
+
+function getSevenDaysAgo() {
+  var now = new Date();
+  var newDate = new Date(now.getTime() - (7*24*60*60*1000));
+  var day = newDate.getDate();
+  var month = newDate.getMonth()+1;
+  var year = newDate.getFullYear();
+  if (month < 10) {
+    month = '0' + month;
+  }
+  else {
+    month = '' + month;
+  }
+  if (day < 10) {
+    day = '0' + day;
+  }
+  else {
+    day = '' + day;
+  }
+  year = '' + year;
+
+  return year + month + day;
+}
+
+function getYesterday() {
+  var now = new Date();
+  var newDate = new Date(now.getTime() - (1*24*60*60*1000));
+  var day = newDate.getDate();
+  var month = newDate.getMonth()+1;
+  var year = newDate.getFullYear();
+  if (month < 10) {
+    month = '0' + month;
+  }
+  else {
+    month = '' + month;
+  }
+  if (day < 10) {
+    day = '0' + day;
+  }
+  else {
+    day = '' + day;
+  }
+  year = '' + year;
+
+  return year + month + day;
+}
+
+function getChartData(search_input) {
+  sevenDaysAgo = getSevenDaysAgo();
+  yesterday = getYesterday();
+
+  let response;
+  let prices = [];
+  let dates = [];
+  let url = 'https://marketdata.websol.barchart.com/getHistory.json?apikey=5077e2ecc61ff495ec2387ec9e58cf95&symbol=' + search_input + '&type=daily&startDate=' + sevenDaysAgo + '&endDate=' + yesterday + '&order=asc';
+  $.getJSON(url, function(result){
+    response = result.results;
+    for (let i = 0; i < response.length; i++) {
+      prices.push(response[i].close);
+      dates.push(response[i].tradingDay);
+    }
+  });
+  return {"prices":prices, "dates": dates};
+}
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+  // *     example: number_format(1234.56, 2, ',', ' ');
+  // *     return: '1 234,56'
+  number = (number + '').replace(',', '').replace(' ', '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
+// Area Chart Example
+
+var ctx = document.getElementById("myLineChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: values.dates,
+    datasets: [{
+      label: "Price",
+      lineTension: 0.3,
+      backgroundColor: "rgba(78, 115, 223, 0.05)",
+      borderColor: "rgba(78, 115, 223, 1)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointBorderColor: "rgba(78, 115, 223, 1)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: values.prices,
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return '$' + number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+        }
+      }
+    }
+  }
+});
+
+setTimeout(function() { myLineChart.update(); },500);
+
+
+  </script>
 
 </body>
 
