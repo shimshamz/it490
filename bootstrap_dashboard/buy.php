8b2161a42fb1f75b8ce18bda4d6eedfa5b8d3c48
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-23 12:27:30 -0400

    Pasted working buy file code

--- bootstrap_dashboard/buy.php
+++ bootstrap_dashboard/buy.php
@@ -1,7 +1,7 @@
+    
 <?php
 session_start();
 $userid = $_SESSION['userid'];
-
 $mydb = new mysqli('127.0.0.1','admin','password','stocks');
 if ($mydb->errno != 0)
 {
@@ -9,28 +9,44 @@ if ($mydb->errno != 0)
         exit(0);
 }
 echo "successfully connected to database".PHP_EOL;
-
-$userQuery = mysqli_query($mydb,"SELECT * FROM user WHERE id = '$userid'");
-$user = mysqli_fetch_array($userQuery, MYSQLI_ASSOC);
-$userid = $user['id'];
-
+# $query = mysqli_query($mydb,"INSERT INTO portfolio (dashboard_id, comany_symbol, company_name, volume, buy_date, buy_amnt, current_amnt, sell_date, sell_amnt) VALUES ('111', 'test0', 'test', '3', NOW(), '32', '32', NOW(), '32')");
+$dash_info = mysqli_query($mydb,"SELECT * FROM dashboard WHERE userid = $userid");
+#$quantity = '3';
+#$symbol = '3';
+#$company_name = 'test';
+#$price = '3';
+#$dashboard_id = 'test';
+$currBal = '3';
+#$totalAmount = '3';
 $quantity = $_POST['buy'];
 $symbol = $_SESSION['symbol'];
 $company_name = $_SESSION['name'];
 $price = $_SESSION['currPrice'];
-$currBal = $user['balance'];
-
+#$request['dashboard_id'] = $dash_info['balance'];
+$currBal = $_SESSION['currBal'];
 $totalAmount = $quantity * $price;
-
-if ($totalAmount > $currBal) {
-  echo "Total buy amount exceeds balance available."
+#$request['quantity'] = $_POST['buy'];
+#$request ['symbol'] = $_SESSION['symbol'];
+#$request['company_name'] = $_SESSION['name'];
+#$request['price'] = $_SESSION['currPrice'];
+#$request['dashboard_id'] = $dash_info['balance'];
+#$request['currBal'] = $_SESSION['currBal'];
+#$request['totalAmount'] = $request['quantity']* $request['price'];
+echo "Total quantit : $request[quantity]";
+echo "Total Pric $request[totalAmount]";
+#$query = mysqli_query($mydb,"INSERT INTO portfolio (id, dashboard_id, comany_symbol, company_name, volume, buy_date, buy_amnt, current_amnt, sell_date, sell_amnt) VALUES ('1', '1' 'test0', 'test', '3', NOW(), '32', '32', NOW(),'32')");
+#$query = mysqli_query($mydb,"INSERT INTO portfolio (dashboard_id, comany_symbol, company_name, volume, buy_date, buy_amnt, current_amnt, sell_date, sell_amnt) VALUES ('56', '$symbol', '$company_name', '$quantity', CURDATE(), '$price', '$newBal', NOW(), NULL)");
+#$query = mysqli_query($mydb,"INSERT INTO portfolio (dashboard_id, comany_symbol, company_name, volume, buy_date, buy_amnt, current_amnt, sell_date, sell_amnt) VALUES ('56', '$request[symbol]', '$request[company_name]', '$request[quantity]', CURDATE(), '$request[price]', '$request[currBal]', NOW(), NULL)");
+if ($totalAmount  > $currBal ) {
+  echo "Total buy amount exceeds balance available.";
 }
 else {
-  $query = mysqli_query($mydb, "INSERT INTO portfolio (user_id, company_symbol, company_name, volume, buy_date, buy_amnt) VALUES ('$userid', '$symbol', '$company_name', '$quantity', CURDATE(), '$price')");
   $newBal = $currBal - $totalAmount;
-  $balanceUpdate = mysqli_query($mydb, "UPDATE user SET balance = $newBal WHERE id = $id");
+  $dash_info = mysqli_query($mydb,"UPDATE dashboard SET balance = $newBal WHERE userid = 8");
   
-  header('Location: index.php');
+ # $query = mysqli_query($mydb,"INSERT INTO portfolio (dashboard_id, comany_symbol, company_name, volume, buy_date, buy_amnt, current_amnt, sell_date, sell_amnt) VALUES ('56', '$symbol', '$company_name', '$quantity', CURDATE(), '$price', '$newBal', NOW(), NULL)");
+  
+  echo "New Balance = $newBal";  
+#  header('Location: index.php');
 }
-
 ?>
