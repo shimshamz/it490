d5d951abc6fa969d6a848b2c62aeb6e37c6ab4d4
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-24 14:10:27 -0400

    Made mydb global variable

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
 
-$mydb = new mysqli('127.0.0.1','admin','password','stocks');
+global $mydb = new mysqli('127.0.0.1','admin','password','stocks');
 if ($mydb->errno != 0)
 {
         echo "failed to connect to database: ". $mydb->error . PHP_EOL;
         exit(0);
 }
-echo "successfully connected to database".PHP_EOL;
+//echo "successfully connected to database".PHP_EOL;
 
 ?>
