6df2ae62023a3dc9835e7885f2ae6b10ac4f04e1
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-23 20:48:22 -0400

    Added update query to buy

--- bootstrap_dashboard/buy.php
+++ bootstrap_dashboard/buy.php
@@ -34,7 +34,7 @@ else {
   $portfolio_info = mysqli_query($mydb,"SELECT * FROM portfolio WHERE user_id = '$userid' AND company_symbol = '$symbol'");
   if (mysqli_num_rows($portfolio_info) == 0) {
     $query = mysqli_query($mydb,"INSERT INTO portfolio (user_id, company_symbol, company_name, volume, buy_date, buy_amnt, sell_date, sell_amnt) VALUES ('$userid', '$symbol', '$company_name', '$quantity', CURDATE(), '$totalAmount', NULL, NULL)");
-    $user_info = mysqli_query($mydb,"UPDATE user SET balance = $newBal WHERE id='$userid'");
+    $user_info = mysqli_query($mydb,"UPDATE user SET balance = '$newBal' WHERE id='$userid'");
   }
   else {
     $info = mysqli_fetch_array($portfolio_info, MYSQLI_ASSOC);
@@ -44,6 +44,7 @@ else {
     $newVolume = $currVolume + $quantity;
     $newBuyAmnt = $currBuyAmnt + $totalAmount;
     $query = mysqli_query($mydb,"UPDATE portfolio SET volume='$newVolume', buy_amnt='$newBuyAmnt' WHERE id='$portfolioId')");
+    $user_info = mysqli_query($mydb,"UPDATE user SET balance = '$newBal' WHERE id='$userid'");
   }
   
   
