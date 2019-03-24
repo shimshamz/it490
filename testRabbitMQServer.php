c60996c97d5d4317d4c0cb382a20c389e0cf1640
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-03-24 14:04:06 -0400

    Included database.php

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -3,80 +3,60 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+include('database.php');
 
 function doLogin($email, $password)
 {	
-$mydb = new mysqli('127.0.0.1','admin','password','stocks');
-
-if ($mydb->errno != 0)
-{
-        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
-        exit(0);
-}
-
-echo "successfully connected to database".PHP_EOL;
-	$query = mysqli_query($mydb, "SELECT * FROM user WHERE email = '$email' AND password = '$password'");
-	$count = mysqli_num_rows($query);
-	//Check if credentials match the database
-	if ($count == 1){
-			//Match
-			echo "<br><br>USERS CREDENTIALS VERIFIED";
-			return true;
-		}else{
-			//No Match
+  $query = mysqli_query($mydb, "SELECT * FROM user WHERE email = '$email' AND password = '$password'");
+  $count = mysqli_num_rows($query);
+  //Check if credentials match the database
+  if ($count == 1){
+  		//Match
+  		echo "<br><br>USERS CREDENTIALS VERIFIED";
+  		return true;
+  	}else{
+  		//No Match
                 	echo "<br><br>Mehta Suck!!!!";
         	        return false;
-		}
+  	}
 		
-if ($mydb->errno != 0)
-{
-        echo "failed to execute query:".PHP_EOL;
-        echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
-        exit(0);
-}
+  if ($mydb->errno != 0)
+  {
+          echo "failed to execute query:".PHP_EOL;
+          echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+          exit(0);
+  }
 
 }
 
 
 function doregister($fname, $lname, $email, $password)
 {
-$mydb = new mysqli('127.0.0.1','admin','password','stocks');
+  $query = mysqli_query($mydb,"SELECT * FROM user WHERE email = '$email' AND password = '$password'");
+  $count = mysqli_num_rows($query);
 
-if ($mydb->errno != 0)
-{
-        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
-        exit(0);
-}
+  //Check if credentials match the database
+  if ($count == 1){
+    //Match
+    echo "<br><br>Please register with differernt email";
+    return true;
+  }else{
+    //No Match
+    $query = mysqli_query($mydb, "INSERT INTO user (fname, lname, email, password) VALUES ('$fname','$lname','$email','$password')");
+    $userQuery = mysqli_query($mydb, "SELECT id FROM user WHERE email = '$email'");
+    $user = mysqli_fetch_array($userQuery, MYSQLI_ASSOC);
+    $userid = $user['id'];
 
-echo "successfully connected to database".PHP_EOL;
+    echo "<br><br>Register Successful!!!!";
+    return false;
+  }
 
-$query = mysqli_query($mydb,"SELECT * FROM user WHERE email = '$email' AND password = '$password'");
-$count = mysqli_num_rows($query);
-
-//Check if credentials match the database
-if ($count == 1){
-  //Match
-  echo "<br><br>Please register with differernt email";
-  return true;
-}else{
-  //No Match
-  $query = mysqli_query($mydb, "INSERT INTO user (fname, lname, email, password) VALUES ('$fname','$lname','$email','$password')");
-  $userQuery = mysqli_query($mydb, "SELECT id FROM user WHERE email = '$email'");
-  $user = mysqli_fetch_array($userQuery, MYSQLI_ASSOC);
-  $userid = $user['id'];
-
-  echo "<br><br>Register Successful!!!!";
-  return false;
-}
-
-if ($mydb->errno != 0)
-{
-        echo "failed to execute query:".PHP_EOL;
-        echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
-        exit(0);
-}
-
-
+  if ($mydb->errno != 0)
+  {
+          echo "failed to execute query:".PHP_EOL;
+          echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+          exit(0);
+  }
 
 }
 
